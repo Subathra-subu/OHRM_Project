86fb17ea9d67426d6108d20b6888cdb2be7371e6
sriram-Kannan-45 <wavene20@gmail.com>
2026-06-11 19:21:54 +0530

    invalid crendential

--- PROJECT_OHRM/Pages/LoginPage.py
+++ PROJECT_OHRM/Pages/LoginPage.py
@@ -2,12 +2,18 @@ from selenium.webdriver.common.by import By
 
 class LoginPage:
 
-    USERNAME = (By.XPATH , "//input[@name='username']" )
+    USERNAME = (By.XPATH, "//input[@name='username']")
 
-    PASSWORD = (By.XPATH , "//input[@name='password']" )
+    PASSWORD = (By.XPATH, "//input[@name='password']")
 
-    LOGIN_BUTTON = (By.XPATH , "//button[@type='submit']" )
+    LOGIN_BUTTON = (By.XPATH, "//button[@type='submit']")
 
-    INVALID_CREDENTIALS_MESSAGE = (By.XPATH , "//div[@role='alert']//descendant::p" )
+    INVALID_CREDENTIALS_MESSAGE = (
+        By.XPATH,
+        "//div[@role='alert']//descendant::p"
+    )
 
-    
+    REQUIRED_MESSAGE = (
+        By.XPATH,
+        "//span[contains(@class,'oxd-input-field-error-message')]"
+    )
