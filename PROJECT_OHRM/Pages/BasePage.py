eb8b65ae0d00ab842688034160624c468445f2ce
krishnaprasath <prasathkrishna002@gmail.com>
2026-06-11 10:33:24 +0530

    updated locators

--- PROJECT_OHRM/Pages/BasePage.py
+++ PROJECT_OHRM/Pages/BasePage.py
@@ -3,16 +3,16 @@ from selenium.webdriver.common.by import By
 
 class BasePage:
 
-    search      = "//input[@class='oxd-input oxd-input--active']"
-    Admin       = "//li[@class='oxd-main-menu-item-wrapper']/child::a[contains(@href,'admin')]" 
-    PIM         = "//li[@class='oxd-main-menu-item-wrapper']/child::a[contains(@href,'viewPim')]"
-    Leave       = "//li[@class='oxd-main-menu-item-wrapper']/child::a[contains(@href,'leave')]"
-    Time        = "//li[@class='oxd-main-menu-item-wrapper']/child::a[contains(@href,'time')]"
-    Recruitment = "//li[@class='oxd-main-menu-item-wrapper']/child::a[contains(@href,'recruitment')]"
-    my_info     = "//li[@class='oxd-main-menu-item-wrapper']/child::a[contains(@href,'viewMyDetails')]"
-    performance = "//li[@class='oxd-main-menu-item-wrapper']/child::a[contains(@href,'performance')]"
-    Dashboard   = "//li[@class='oxd-main-menu-item-wrapper']/child::a[contains(@href,'dashboard')]"
-    Directory   = "//li[@class='oxd-main-menu-item-wrapper']/child::a[contains(@href,'direct')]"
-    Maintenance = "//li[@class='oxd-main-menu-item-wrapper']/child::a[contains(@href,'maintenance')]"  
-    claim       = "//li[@class='oxd-main-menu-item-wrapper']/child::a[contains(@href,'clai')]"
-    Buzz        = "//li[@class='oxd-main-menu-item-wrapper']/child::a[contains(@href,'buzz')]"
+    search = (By.XPATH,"//input[@class='oxd-input oxd-input--active']")
+    Admin = (By.XPATH,"//li[@class='oxd-main-menu-item-wrapper']/child::a[contains(@href,'admin')]") 
+    PIM = (By.CSS_SELECTOR,"a[href*='viewPim']")
+    Leave = (By.XPATH,"//li[@class='oxd-main-menu-item-wrapper']/child::a[contains(@href,'leave')]")
+    Time = (By.XPATH,"//li[@class='oxd-main-menu-item-wrapper']/child::a[contains(@href,'time')]")
+    Recruitment = (By.XPATH,"//li[@class='oxd-main-menu-item-wrapper']/child::a[contains(@href,'recruitment')]")
+    my_info = (By.CSS_SELECTOR,"a[href*='viewMyDe']")
+    performance = (By.XPATH,"//li[@class='oxd-main-menu-item-wrapper']/child::a[contains(@href,'performance')]")
+    Dashboard = (By.XPATH,"//li[@class='oxd-main-menu-item-wrapper']/child::a[contains(@href,'dashboard')]")
+    Directory = (By.XPATH,"//li[@class='oxd-main-menu-item-wrapper']/child::a[contains(@href,'direct')]")
+    Maintenance = (By.XPATH,"//li[@class='oxd-main-menu-item-wrapper']/child::a[contains(@href,'maintenance')]")  
+    claim = (By.XPATH,"//li[@class='oxd-main-menu-item-wrapper']/child::a[contains(@href,'clai')]")
+    Buzz = (By.XPATH,"//li[@class='oxd-main-menu-item-wrapper']/child::a[contains(@href,'buzz')]")
--- PROJECT_OHRM/Pages/PIMpage.py
+++ PROJECT_OHRM/Pages/PIMpage.py
@@ -3,12 +3,12 @@ from selenium.webdriver.common.by import By
 
 class PIMpage:
      
-     addemployee_btn  = "//div[@class='oxd-topbar-body']/descendant::a[2]"
-     fname            = "//input[@name='firstName']"
-     mname            = "//input[@name='middleName']"
-     lname            = "//input[@name='lastName']"
-     create_lgn_dts   = "//input[@type='checkbox']//following-sibling::span"
-     username         = "(//div[contains(@class,'oxd-input-group')]//input[@autocomplete='off'])[1]"
-     password         = "(//div[contains(@class,'oxd-input-group')]//input[@type='password'])[1]"
-     confirm_password = "(//div[contains(@class,'oxd-input-group')]//input[@type='password'])[2]"
-     save_btn         = "//div[@class='oxd-form-actions']/child::button[@type='submit']"
+     addemployee_btn  = (By.XPATH, "//div[@class='oxd-topbar-body']/descendant::a[2]")
+     fname            = (By.XPATH, "//input[@name='firstName']")
+     mname            = (By.XPATH, "//input[@name='middleName']")
+     lname            = (By.XPATH, "//input[@name='lastName']")
+     create_lgn_dts   = (By.XPATH, "//input[@type='checkbox']//following-sibling::span")
+     username         = (By.XPATH, "(//div[contains(@class,'oxd-input-group')]//input[@autocomplete='off'])[1]")
+     password         = (By.XPATH, "(//div[contains(@class,'oxd-input-group')]//input[@type='password'])[1]")
+     confirm_password = (By.XPATH, "(//div[contains(@class,'oxd-input-group')]//input[@type='password'])[2]")
+     save_btn         = (By.XPATH, "//div[@class='oxd-form-actions']/child::button[@type='submit']")
