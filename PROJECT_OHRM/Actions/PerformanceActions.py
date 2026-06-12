abe318a90fa9d04019b8fcae9c0394a2efc88319
Shobana <shobanav112@gmail.com>
2026-06-12 14:14:54 +0530

    Added with invalid without job title in kpi

--- PROJECT_OHRM/Actions/PerformanceActions.py
+++ PROJECT_OHRM/Actions/PerformanceActions.py
@@ -19,6 +19,7 @@ class PerformanceActions(BaseActions):
         self.js_click(PerformancePage.click_title)
         self.js_click(PerformancePage.submit)
         return self.get_text(PerformancePage.success_msg)
+    
     def invalid_add(self,kipinvalid):
         actions = ActionChains(self.driver)
         self.click(BasePage.performance)
@@ -30,7 +31,38 @@ class PerformanceActions(BaseActions):
         actions.send_keys(Keys.ARROW_DOWN).send_keys(Keys.ENTER).perform()
         self.js_click(PerformancePage.click_title)
         self.js_click(PerformancePage.submit)
-        return self.get_text(PerformancePage.required)
+        return self.get_text(PerformancePage.kpi_required)
+    
+    def invalid_addwithtitle(self,kip):
+        self.click(BasePage.performance)
+        self.js_click(PerformancePage.configure)
+        self.click(PerformancePage.kip_select)
+        self.js_click(PerformancePage.add)
+        self.enter_text(PerformancePage.kip,kip)
+        self.js_click(PerformancePage.submit)
+        return self.get_text(PerformancePage.job_required)
+
+    def invalid_max_rate(self,kip,maxrate):
+        actions = ActionChains(self.driver)
+        self.click(BasePage.performance)
+        self.js_click(PerformancePage.configure)
+        self.click(PerformancePage.kip_select)
+        self.js_click(PerformancePage.add)
+        self.enter_text(PerformancePage.kip,kip)
+        self.click(PerformancePage.job_title)
+        actions.send_keys(Keys.ARROW_DOWN).send_keys(Keys.ENTER).perform()
+        self.js_click(PerformancePage.click_title)
+        self.clear(PerformancePage.max_rate)
+        self.enter_text(PerformancePage.max_rate,maxrate)
+        self.js_click(PerformancePage.submit)
+        return self.get_text(PerformancePage.max_err)
+
+
+
+
+
+
+
 
 
         
--- PROJECT_OHRM/Tests/test_performance.py
+++ PROJECT_OHRM/Tests/test_performance.py
@@ -57,6 +57,47 @@ class Test_Performance:
 
             self.logger.error(f"Test Failed: {e}")
             raise
+    def test_invalidwithouttitle(self):
+        try:
+            self.logger.info("Scenario started")
+            login = LoginActions(self.driver, self.wait)
+
+            login.login(
+                get_config("username and password", "username"),
+                get_config("username and password", "password")
+            )
+
+            kpiinvalid = get_config("performance", "kpi")
+            performance = PerformanceActions(self.driver, self.wait)
+            res=performance.invalid_addwithtitle(kpiinvalid)
+            assert res == "Required"
+            self.logger.info("KPI Not Added Successfully")
+        except Exception as e:
+
+            self.logger.error(f"Test Failed: {e}")
+            raise
+    def test_invalidmax_rate(self):
+        try:
+            self.logger.info("Scenario started")
+            login = LoginActions(self.driver, self.wait)
+
+            login.login(
+                get_config("username and password", "username"),
+                get_config("username and password", "password")
+            )
+
+            kpiinvalid = get_config("performance", "kpi")
+            max_rate = get_config("performance","maxrate")
+
+            performance = PerformanceActions(self.driver, self.wait)
+            res=performance.invalid_max_rate(kpiinvalid,max_rate)
+            assert res == "Should be a number between 0-100"
+            self.logger.info("KPI Not Added Successfully")
+        except Exception as e:
+
+            self.logger.error(f"Test Failed: {e}")
+            raise
+
           
 
 
