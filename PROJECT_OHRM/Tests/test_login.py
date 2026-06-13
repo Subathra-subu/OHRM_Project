5b9539f5bf43105572b2678fc1c1160e0fd14477
sriram-Kannan-45 <wavene20@gmail.com>
2026-06-13 23:53:13 +0530

    test datas are modified

--- PROJECT_OHRM/Tests/test_login.py
+++ PROJECT_OHRM/Tests/test_login.py
@@ -1,3 +1,5 @@
+
+import os
 import pytest
 
 from Actions.LoginActions import LoginActions
@@ -5,18 +7,27 @@ from Actions.DashboardActions import DashboardActions
 
 from Utilities.ReadConfig import get_config
 from Utilities.Logger import log_generator
-
 from Utilities import ExcelUtils
+
+
+invalid_login_path = os.path.join(
+    os.path.dirname(__file__),
+    "..",
+    "test_data",
+    "inValid_Login_datas.xlsx"
+)
+
+
 @pytest.mark.usefixtures("test_setup_and_down")
 class Test_Login:
 
     logger = log_generator()
-    
+
     def test_login(self):
 
         try:
 
-            self.logger.info("Test Started")
+            self.logger.info("Login Test Started")
 
             username = get_config(
                 "username and password",
@@ -45,11 +56,19 @@ class Test_Login:
 
             dashboard.verify_dashboard()
 
-            self.logger.info("Test Passed")
+            self.logger.info(
+                "Login Test Passed"
+            )
 
         except Exception as e:
 
-            self.logger.error("Test Failed")
+            self.logger.error(
+                "Login Test Failed"
+            )
+
+            self.save_screenshot(
+                "login_test_failed"
+            )
 
             self.logger.exception(e)
 
@@ -57,50 +76,69 @@ class Test_Login:
 
 
     @pytest.mark.parametrize(
-    "username,password",
-    ExcelUtils.get_data(
-        r"test_data\inValid_Login_datas.xlsx",
-        "Sheet1"
+        "username,password",
+        ExcelUtils.get_data(
+            invalid_login_path,
+            "Sheet1"
         )
     )
-    def test_invalid_login(self, username, password):
+    def test_invalid_login(
+            self,
+            username,
+            password):
 
         try:
 
-            self.logger.info("Invalid Login Test Started")
+            self.logger.info(
+                "Invalid Login Test Started"
+            )
 
             login = LoginActions(
-            self.driver,
-            self.wait
-        )
+                self.driver,
+                self.wait
+            )
 
             login.invalid_login(
-            username,
-            password
-        )
+                username,
+                password
+            )
 
-            self.logger.info("Invalid Login Test Passed")
+            self.logger.info(
+                "Invalid Login Test Passed"
+            )
 
         except Exception as e:
 
-            self.logger.error("Invalid Login Test Failed")
+            self.logger.error(
+                "Invalid Login Test Failed"
+            )
+
+            self.save_screenshot(
+                "invalid_login_test_failed"
+            )
 
             self.logger.exception(e)
 
             raise
 
+
     @pytest.mark.parametrize(
-    "username,password",
-    ExcelUtils.get_data(
-        r"test_data\inValid_Login_datas.xlsx",
-        "Sheet2"
+        "username,password",
+        ExcelUtils.get_data(
+            invalid_login_path,
+            "Sheet2"
         )
     )
-    def test_without_credential(self, username, password):
+    def test_without_credential(
+            self,
+            username,
+            password):
 
         try:
 
-            self.logger.info("Without Credential Test Started")
+            self.logger.info(
+                "Without Credential Test Started"
+            )
 
             login = LoginActions(
                 self.driver,
@@ -112,12 +150,21 @@ class Test_Login:
                 password
             )
 
-            self.logger.info("Without Credential Test Passed")
+            self.logger.info(
+                "Without Credential Test Passed"
+            )
 
         except Exception as e:
 
-            self.logger.error("Without Credential Test Failed")
+            self.logger.error(
+                "Without Credential Test Failed"
+            )
+
+            self.save_screenshot(
+                "without_credential_test_failed"
+            )
 
             self.logger.exception(e)
 
             raise
+
