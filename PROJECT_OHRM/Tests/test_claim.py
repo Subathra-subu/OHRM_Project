2c089b588c8bfc34e89a0ae31b48061950ea6095
Subathra-subu <subathrasubu2004@gmail.com>
2026-08-17 21:16:34 +0530

    Submit Assign scenario added

--- PROJECT_OHRM/Tests/test_claim.py
+++ PROJECT_OHRM/Tests/test_claim.py
@@ -4,7 +4,7 @@ from Utilities.Logger import log_generator
 
 @pytest.mark.usefixtures("test_setup_and_down")
 
-class TestAssignClaim():
+class TestAssignClaim:
     
     logger = log_generator()
     
@@ -19,3 +19,15 @@ class TestAssignClaim():
         assert claimAction.assign_claim()==True
         
         self.logger.info("Test Ended")
+        
+    def test_submitClaim_valid(self):
+            
+            self.logger.info("Test Started")
+            
+            claimAction = ClaimActions(self.driver,self.wait)
+            
+            claimAction.login_entervacancy()
+            
+            assert claimAction.submitClaim()==True
+            
+            self.logger.info("Test Ended")
